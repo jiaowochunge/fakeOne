d99dd7a1fb62bd50b66ab114a3009f6e26a43c0c
王益 <wangyi@wangyideMac-mini.local>
2015-04-09 16:07:37 +0800

    Initial Commit

---
+++ tabTest/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  tabTest
+//
+//  Created by 王益 on 15/4/9.
+//  Copyright (c) 2015年 test. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ tabTestTests/tabTestTests.swift
@@ -0,0 +1,36 @@
+//
+//  tabTestTests.swift
+//  tabTestTests
+//
+//  Created by 王益 on 15/4/9.
+//  Copyright (c) 2015年 test. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class tabTestTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
